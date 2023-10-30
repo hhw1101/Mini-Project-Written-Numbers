646be82659f1eddd87167b6fd935f0b09182e94d
hhw1101 <jenniferhuang2002@gmail.com>
2023-10-30 06:30:30 +0000

    finished project

--- miniProject_v2.c
+++ miniProject_v2.c
@@ -9,10 +9,10 @@ int main()
 {
     // Defining strings for the numbers
     char digits[9][6] = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
-    char special[9][10] = {"eleven", "tweleve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
-    char tens[9][8] = {"ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"};
-    char denominations[4][9] = {"and", "hundred", "thousand", "million"};
-    char writtenNumber[150];
+    char special[10][10] = {"ten", "eleven", "tweleve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
+    char tens[8][8] = {"twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"};
+    char denominations[4][9] = {"and", "million", "thousand", "hundred"};
+    char writtenNumber[150] = "";
 
     // Defining integer and integer string for input
     unsigned long int input;
@@ -34,64 +34,62 @@ int main()
         {
             separatedInput[i - 1][j - 1] = input / power(10, 10 - i * 3 - j + 3 - 1) % 10;
         }
-        if (separatedInput[i - 1][0] == 0 && separatedInput[i - 1][1] == 0 && separatedInput[i - 1][2] == 0)
-        {
-            is_empty[i - 1] = 1;
-        }
     }
-
     for (int i = 0; i < 3; i++)
     {
-        if (is_empty[i] == 1)
+        if (separatedInput[i][0] == 0 && separatedInput[i][1] == 0 && separatedInput[i][2] == 0)
+        {
+            is_empty[i] = 1; // This is a testing condition and is redundant
+            continue;
+        }
+        else
         {
             for (int j = 0; j < 3; j++)
             {
-                if (j == 0 && separatedInput[i][j] !=0)
+                switch (j)
                 {
-                    strcat(writtenNumber, digits[separatedInput[i][j] - 1]);
-                    strcat(writtenNumber, " ");
-                    strcat(writtenNumber, denominations[1]);
-                }
-                else if (j == 1)
-                {
-                    if (separatedInput[i][j] == 1)
-                    {
-                        strcat(writtenNumber, special[separatedInput[i][j] - 1]);
-                    }
-                    else if (separatedInput[i][j] == 0 && separatedInput[i][j - 1] != 0)
-                    {
-                        strcat(writtenNumber, denominations[0]);
-                    }
-                    else
-                    {
-                        strcat(writtenNumber, tens[separatedInput[i][j] - 1]);
-                    }
-                }
-                else
-                {
+                case 0:
                     if (separatedInput[i][j] != 0)
                     {
                         strcat(writtenNumber, digits[separatedInput[i][j] - 1]);
+                        strcat(writtenNumber, " ");
+                        if (i != 3)
+                        {
+                            strcat(writtenNumber, denominations[3]);
+                            strcat(writtenNumber, " ");
+                        }
                     }
-                }
-                strcat(writtenNumber, " ");
-            }
+                    break;
+                case 1:
+                    if (separatedInput[i][j] == 1)
+                    {
+                        strcat(writtenNumber, special[separatedInput[i][j + 1]]);
+                    }
+                    else if (separatedInput[i][j] != 0)
+                    {
+                        strcat(writtenNumber, tens[separatedInput[i][j] - 2]);
+                    }
 
-            if (i == 0)
-            {
-                strcat(writtenNumber, denominations[3]);
+                    strcat(writtenNumber, " ");
+                    break;
+                case 2:
+                    if (separatedInput[i][j] != 0)
+                    {
+                        strcat(writtenNumber, digits[separatedInput[i][j] - 1]);
+                        strcat(writtenNumber, " ");
+                    }
+                    break;
+                }
             }
-            else if (i == 1)
+            if (i != 2)
             {
-                strcat(writtenNumber, denominations[2]);
+                strcat(writtenNumber, denominations[i + 1]);
             }
             strcat(writtenNumber, " ");
         }
     }
-
     printf("The number you entered, %d is written as:\n%s", input, writtenNumber);
 }
-
 // Power function the finds the power of a number as an integer rather than built in function that returns a double
 int power(int base, int power)
 {
